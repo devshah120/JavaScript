0f37bdfc5cc77a69ec3001785243a1c1ab8f410d
Dev Shah <devshah120902@gmail.com>
2022-11-24 18:54:51 +0530

    use var keyword

---
+++ 02Basic/variables.js
@@ -0,0 +1,22 @@
+//how to name variables
+
+//  fullName  - camel case -  best Practices
+//  full-name - using desh
+//  full_name - snake case
+//  FullName  - Capitalizable - Not Good but Not Giving Errors
+
+var fullName = "Dev Shah";
+
+var courseName = "JavaScript";
+console.log(courseName);
+
+var isLoggedIn = false;
+console.log(isLoggedIn);
+
+var loggedCount = 34;
+console.log(loggedCount);
+
+var paymentMode; //Forward Declaration of Variable
+console.log(paymentMode); //Undefined or Null Value
+
+paymentMode = "Credit Card";
