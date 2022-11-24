ef0bd92711fe985787a06e3b02ec6088aa01e27f
Dev Shah <devshah120902@gmail.com>
2022-11-25 01:24:05 +0530

    ternary operator

---
+++ 02Basic/ternary.js
@@ -0,0 +1,6 @@
+// show user a  signout button if he is authenticated
+// other wise show him option to Login/Signup
+
+var authenticated = true;
+
+authenticated ? console.log("signout") : console.log("Login");
