2d09a616734e52000473ec00a2a5b0060daa2007
Dev Shah <devshah120902@gmail.com>
2022-12-05 23:55:07 +0530

    fill & filter method

---
+++ 03Intermediate/filterArray.js
@@ -0,0 +1,9 @@
+var testArray = [2,4,5,6,7,8,9,1,3]
+
+console.log(testArray.fill("d",2,5));
+
+const myNumber = [23,34,54,23,54,33,56,66,75,85];
+
+const result = myNumber.filter((num)=> num < 54)
+
+console.log(result);
