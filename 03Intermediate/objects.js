75d981fa2dc1787e089bbe87bf60db81e2938089
Dev Shah <devshah120902@gmail.com>
2022-12-06 00:55:24 +0530

    Objects

---
+++ 03Intermediate/objects.js
@@ -0,0 +1,15 @@
+var user = {
+    firstName: "Dev",
+    lastName: "Shah",
+    role: "admin",
+    loginCount: 33,
+    facebookSignedIn: true
+};
+
+console.log(user);
+
+console.log(user.firstName);
+
+user.loginCount =45;
+
+console.table(user)
