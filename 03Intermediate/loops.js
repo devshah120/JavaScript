1c6ae5639f67ac0c5b9ef1f81ef4be5f209c722e
Dev Shah <devshah120902@gmail.com>
2022-12-17 20:15:09 +0530

    for loop with array

--- 03Intermediate/loops.js
+++ 03Intermediate/loops.js
@@ -1,4 +1,14 @@
-for (let i = 0; i <=10; i++) {
-    console.log(i);;
+// for (let i = 0; i <=10; i++) {
+//     console.log(i);  
+// }
+
+var fruit=["apple","mango","kivi",322,"banana"];
+    
+for(let i=0;i<fruit.length;i++)
+{
+    if (typeof fruit[i] == "string")
+    {
+        console.log(fruit[i]);
+    }
     
 }
