97867ed0c5acac9820102f63f4f780b35c82f2cd
Dev Shah <devshah120902@gmail.com>
2022-12-17 20:33:14 +0530

    do while loop

---
+++ 03Intermediate/dowhile.js
@@ -0,0 +1,15 @@
+let i = 0;
+
+var fruit=["apple","mango","kivi",322,"banana",101];
+
+do {
+    console.log(fruit[i]);
+    i++
+} while (i<fruit.length);
+
+
+i = 20
+do {
+    console.log("i am wrong but i am in do while");
+    i++
+} while (i< 10);
