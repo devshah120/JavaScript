5ce54e1b7a8fdd6bdfb5ebbdc951035fe032bfe8
Dev Shah <devshah120902@gmail.com>
2022-12-26 14:09:07 +0530

    Lexical Scoping

---
+++ 05Advance/lexicalScoping.js
@@ -0,0 +1,12 @@
+function init(){
+    var firstName = "Dev";
+    console.log(firstName);
+
+    function sayFirstName() {
+        console.log(firstName);
+    }
+    sayFirstName();
+}
+
+init();
+//console.log(firstName);
