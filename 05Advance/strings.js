57cfce5865fdb2282ec9fc370203a5948fb19d00
Dev Shah <devshah120902@gmail.com>
2022-12-27 16:54:00 +0530

    string literals

---
+++ 05Advance/strings.js
@@ -0,0 +1,11 @@
+var name = `Dev`;
+
+//console.log(name);
+
+var fullName = `${name} Shah`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("h"));
+console.log(fullName.includes("Dev"));
+console.log(fullName.toLocaleUpperCase());
